4f5371c94dadbf6d5af52df1675c49e8a2c1a8c8
luosonglin <luo1992athrun1102@qq.com>
2016-06-07 16:47:53 +0800

    my first ios commit

--- myFirstIOSApp/ViewController.swift
+++ myFirstIOSApp/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var wv:UIWebView!
+    
+    func loadUrl() {
+        wv.loadRequest(NSURLRequest(URL:NSURL(string: "http://luosonglin.github.io")!))
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadUrl()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
